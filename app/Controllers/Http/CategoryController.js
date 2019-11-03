76a7373a29e90ce515f0db029f725da43f724039
Javel Queiroz <javelqueiroz@outlook.com.br>
2019-11-03 16:52:35 -0300

    Modified: update (done)

--- app/Controllers/Http/CategoryController.js
+++ app/Controllers/Http/CategoryController.js
@@ -22,12 +22,27 @@ class CategoryController {
     }
 
 
-    async update({request}){
-        return {};
+    async update({params, request}){
+        const data = request.only(['name', 'category_id']);
+        const category  = await Category.find(params.id)
+
+        category.merge(data)
+         
+        await category.save()
+
+        return category;
     }
 
 
-    
+    async addSubCategory({request}){
+        const data = request.only(['name', 'category_id']);
+
+        const category = await Category.create(data);
+
+        return category;
+        
+    }
+
     async delete({request}){
         return {};
     }
